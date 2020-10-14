ddd753a38d081a41aed4e495ff2f8dd3b1f16305
Ken Dudley <github@dqcs.com>
2020-10-14 02:06:55 -0400

    typescript

---
+++ getver.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var data = require("./myver.json");
+var version = data.cbar;
+console.log(version);
---
+++ getver.ts
@@ -0,0 +1,3 @@
+import * as data from './myver.json';
+let version = data.cbar
+console.log(version);
